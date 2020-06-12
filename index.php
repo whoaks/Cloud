e5b46330017ab8844c0883b7f7ca1b62a9588ca9
whoaks <66693807+whoaks@users.noreply.github.com>
2020-06-12 18:13:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Webhosting using Terraform</title>
+<style>
+body
+{
+background-color:skyblue;
+}
+h1
+{
+color:purple;
+background-color:black;
+color:white;
+padding:10px;
+margin:2em;
+box-shadow:3px 3px 3px 3px yellow;
+}
+</style></head>
+<body>
+<h1>Welcome to My Page</h1>
+<pre>
+<?php
+
+print `ifconfig`;
+
+?>
+
+</pre>
+</body>
+</html>
