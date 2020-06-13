ad138279afd9bf552ba4b6db54e5981b01337b91
whoaks <66693807+whoaks@users.noreply.github.com>
2020-06-13 15:08:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ box-shadow:3px 3px 3px 3px yellow;
 </style></head>
 <body>
 <h1>Welcome to My Page</h1>
-<img src="https://d2lmwni8mtcarg.cloudfront.net/Capture.jpg">
+<img src="https://aks4321.s3.ap-south-1.amazonaws.com/Capture.JPG">
 <pre>
 <?php
 
